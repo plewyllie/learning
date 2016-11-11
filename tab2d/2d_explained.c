650281d808dbd7f476138a001df204c0ca8ea50a
Pieter Lewyllie <plewyllie@gmail.com>
2016-11-11 21:15:12 +0100

    2d comment

---
+++ tab2d/2d_explained.c
@@ -0,0 +1,16 @@
+// THIS IS NOT C!!!
+/* t est un tableau de tableaux == t est un tableau à deux
+ * dimensions
+ */
+
+    int** t;
+
+    int* -> [ int , int , int, int ]
+
+    int** -> [ int*, int*, int*, int*]
+                ->     ->   ->    -> 
+                [ int, [ int, [ int, [ int,
+                  int,   int,   int,   int,
+                  int    int,   int,   int,
+                 ,int ]  int ]  int ]  int ]
+
