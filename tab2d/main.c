38e310e317692fc7e971ed772ce773df38af8dc6
Julien Beckers <beckers.julien@hotmail.com>
2016-11-11 21:21:05 +0100

    Update

--- tab2d/main.c
+++ tab2d/main.c
@@ -9,8 +9,15 @@ int main()
     for (i = 0; i < 5; i++) {
         printf("\n");
         for (j = 0; j < 4; j++) {
+            if(i==2){
+            tab[i][j] = 1;
+            printf("%d", tab[i][j]);
+            }
+            else
+            {
             tab[i][j] = 0;
             printf("%d", tab[i][j]);
+            }
         }
     }
 
